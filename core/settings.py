2714f91aa0d151fb07d182ba5610370d250b4ca5
PikaPika0710 <102190298@sv1.dut.udn.vn>
2022-07-08 15:42:27 +0700

    add static_root

--- core/settings.py
+++ core/settings.py
@@ -169,8 +169,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'media'
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/media/'
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 # Default primary key field type
